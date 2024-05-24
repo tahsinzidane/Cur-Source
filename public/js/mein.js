a764c9039b87e2ebb22f55ae8bc5da6108fe46e1
tahsin <tsnahmed72@.com>
2024-05-24 22:42:16 +0600

    final commti

---
+++ public/js/mein.js
@@ -0,0 +1,7 @@
+// for updating year on footer automaticlly
+const year = document.getElementById('year');
+const currYaer = new Date().getFullYear()
+year.innerText = currYaer;
+
+// auto dark theme
+//  TODO: later
